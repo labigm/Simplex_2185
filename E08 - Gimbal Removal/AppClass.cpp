707ef74a81d33c644c57d637d3db4632618d257e
Seth Lambert <slambert9@fingerlakes.edu>
2019-02-26 01:04:01 -0500

    unsolved

--- E08 - Gimbal Removal/AppClass.cpp
+++ E08 - Gimbal Removal/AppClass.cpp
@@ -1,10 +1,12 @@
 #include "AppClass.h"
+#include <glm/gtx/quaternion.hpp>
 void Application::InitVariables(void)
 {
 	//init the mesh
 	m_pMesh = new MyMesh();
 	//m_pMesh->GenerateCube(1.0f, C_WHITE);
 	m_pMesh->GenerateCone(2.0f, 5.0f, 3, C_WHITE);
+	
 }
 void Application::Update(void)
 {
@@ -22,13 +24,19 @@ void Application::Display(void)
 	// Clear the screen
 	ClearScreen();
 
+	glm::quat orientation = glm::angleAxis(glm::radians(0.0f), vector3(0.0f, 0.0f, 0.0f));
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
+	
+	glm::quat quat_RotationX = glm::angleAxis(glm::radians(m_v3Rotation.x), vector3(1.0f, 0.0f, 0.0f));
+	glm::quat quat_RotationY = glm::angleAxis(glm::radians(m_v3Rotation.y), vector3(0.0f, 1.0f, 0.0f));
+	glm::quat quat_RotationZ = glm::angleAxis(glm::radians(m_v3Rotation.z), vector3(0.0f, 0.0f, 1.0f));
 
-	m_m4Model = glm::rotate(IDENTITY_M4, glm::radians(m_v3Rotation.x), vector3(1.0f, 0.0f, 0.0f));
-	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.y), vector3(0.0f, 1.0f, 0.0f));
-	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.z), vector3(0.0f, 0.0f, 1.0f));
-	m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_m4Model));
+	orientation *= quat_RotationX * quat_RotationY*quat_RotationZ;
+	
+	//m_m4Model = glm::toMat4(quat_RotationX*quat_RotationY*quat_RotationZ);
+	m_m4Model = glm::toMat4(orientation);
+	m_pMesh->Render(m4Projection, m4View, m_m4Model);
 
 	//m_qOrientation = m_qOrientation * glm::angleAxis(glm::radians(1.0f), vector3(1.0f));
 	//m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_qOrientation));
